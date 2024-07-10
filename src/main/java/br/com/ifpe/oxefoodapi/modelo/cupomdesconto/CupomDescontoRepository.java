ea9ad161bbbfc3485f1a286476364711952ec5a6
giovaniflp <giovanifeitosa123@gmail.com>
2024-07-10 01:37:19 -0300

    Atualizando o projeto

---
+++ src/main/java/br/com/ifpe/oxefoodapi/modelo/cupomdesconto/CupomDescontoRepository.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.oxefoodapi.modelo.cupomdesconto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CupomDescontoRepository extends JpaRepository<CupomDesconto, Long> {
+
+}
